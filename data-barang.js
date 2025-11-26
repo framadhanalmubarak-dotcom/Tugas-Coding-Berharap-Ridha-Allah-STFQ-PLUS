c92f11b61681b36d708ed49da87977b872ee0e85
Farras Ramadhan Al-mubarak <framadhanalmubarak@gmail.com>
2025-11-26 14:42:34 +0700

    ....

--- data-barang.js
+++ data-barang.js
@@ -1,31 +1,34 @@
-let dataBarang = JSON.parse(localStorage.getItem("dataBarang")) || [];
+let dataBarang = [];
 tampilkanTabel(); 
 
 $("#btnTambah").click(function () {
-  const nama = $("#namaBarang").val();
-  const harga = $("#harga").val();
-  const jumlah = $("#jumlah").val();
-  const kategori = $("#kategori").val();
+    const nama = $("#namaBarang").val();
+    const harga = $("#harga").val();
+    const jumlah = $("#jumlah").val();
+    const kategori = $("#kategori").val();
 
-  if (!nama || !harga || !jumlah) {
-    alert("Data belum lengkap!");
-    return;
-  }
+    if (!nama || !harga || !jumlah) {
+        alert("Data belum lengkap!");
+        return;
+    }
 
-  const barang = { nama, harga, jumlah, kategori };
-  dataBarang.push(barang);
+    const barang = { nama, harga, jumlah, kategori };
+    dataBarang.push(barang);
+    console.log(dataBarang);
 
-  tampilkanTabel();
-  hapusForm();
+    localStorage.setItem("dataBarang", JSON.stringify(dataBarang));        
+
+    tampilkanTabel();
+    hapusForm();
 });
 
 $("#btnHapus").click(function () {
-  if (confirm("Yakin ingin menghapus semua data?")) {
-    dataBarang = [];
-    localStorage.removeItem("dataBarang");
-    tampilkanTabel();
-  hapusForm();
-  }
+    if (confirm("Yakin ingin menghapus semua data?")) {
+        dataBarang = [];
+        localStorage.removeItem("dataBarang");
+        tampilkanTabel();
+        hapusForm();
+    }
 });
 
 function tampilkanTabel() {
@@ -33,19 +36,27 @@ function tampilkanTabel() {
 
   dataBarang.forEach(item => {
     $("#tabelData").append(`
-      <tr class='border border-gray-700'>
-        <td class="p-2 border">${item.nama}</td>
-        <td class="p-2 border">${item.harga}</td>
-        <td class="p-2 border">${item.jumlah}</td>
-        <td class="p-2 border">${item.kategori}</td>
-      </tr>
+        <tr class='border border-gray-700'>
+            <td class="p-2 border">${item.nama}</td>
+            <td class="p-2 border">${item.harga}</td>
+            <td class="p-2 border">${item.jumlah}</td>
+            <td class="p-2 border">${item.kategori}</td>
+        </tr>
     `);
   });
 }
 
 function hapusForm() {
-  $("#namaBarang").val("");
-  $("#harga").val("");
-  $("#jumlah").val("");
-  $("#kategori").val("peralatan olahraga");
+    $("#namaBarang").val("");
+    $("#harga").val("");
+    $("#jumlah").val("");
+    $("#kategori").val("peralatan olahraga");
+}
+
+window.onload = function() {
+    const storedData = localStorage.getItem("dataBarang");
+    if (storedData) {
+        dataBarang = JSON.parse(storedData);
+        tampilkanTabel();
+    }
 }
